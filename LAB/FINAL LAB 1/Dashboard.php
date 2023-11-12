6c0885c6727e478169b40d5027b7fe46bd004169
Badhon Nath Joy <8ad40n@gmail.com>
2023-11-12 14:52:54 +0600

    Added

---
+++ LAB/FINAL LAB 1/Dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Dashboard.css">
+</head>
+<body>
+    <nav>
+    <table border="1">
+        <ul>
+        <tr>
+            <td><li><a href="#"><h3>Home</h3></a></li></td>
+            <td><li><a href="#"><h3>Dashboard</h3></a></li></td>
+            <td><li><a href="#"><h3>Profit</h3></a></li></td>
+            <td><li><a href="#"><h3>Forget Password</h3></a></li></td>
+        </tr>
+        <tr>
+            <td><li class="hour"><a href="#"><h3>Hour</h3></a></li></td>
+            <td colspan="3" class="blank"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td  colspan="3" class="blank"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td  colspan="3" class="blank"></td>
+        </tr>
+
+    </table>
+    </ul>
+    </nav>
+</body>
+</html>
---
+++ LAB/FINAL LAB 1/login.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+$servername="localhost";
+$username= "root";
+$password="";
+$dbname="project";
+$conn= new mysqli($servername,$username,$password,$dbname);
+if (!$conn) 
+    {
+    die("Connection failed: " . mysqli_connect_error());
+    }    
+if(isset($_POST['login'])) 
+{
+    $id=$_POST['id'];
+    $pass=$_POST['pass'];
+    $sql= "select * from userinfo where ID='$id' and Password='$pass'";
+    $res= mysqli_query($conn,$sql);
+    if (!empty($id) && !empty($pass)){
+    if($res->num_rows==1)
+    {
+        $new=mysqli_fetch_assoc($res);
+        $user=$new["Type"];
+        $_SESSION['id']=$new["ID"];
+
+        if($user=="Admin")
+        {
+            header("location: Dashboard.php");
+        }
+        // elseif($user=="Faculty")
+        // {
+        //     header("location: FacultyHome.php");
+        // }
+
+    }
+    else
+    {
+        echo "<script>alert('Wrong password. Please try again.');</script>";
+    }
+}
+    else
+    {
+        echo "<script>alert('Please fill in all the fields.');</script>";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>  
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <form method="post">
+    <h1>Login</h1>
+    <script>
+    function togglePassword() 
+    {
+    var passwordInput = document.getElementById("pass");
+    if (passwordInput.type == "password") 
+    {
+    passwordInput.type = "text";
+    } 
+    else 
+    {
+    passwordInput.type = "password";
+    }
+    }
+    </script>
+    
+    <label for="id">ID:</label><br> <input type="text" name="id" placeholder="User Name"><br><br>
+    <label for="password">Password:</label> <br><input type="password" name="pass" id="pass" placeholder="Password"><br>
+     <input type="checkbox" id="showPassword" onclick="togglePassword()">
+     <small>Show Password</small><br><br>
+     <button name="login">Login</button><br>
+     <a href="EmailVerification.php">Forgot Password?</a>
+    
+    </form>
+</body>
+</html>
